97bd2c70fb50982e989aed8bea1f73cd4e0e5cc9
dodoza1415 <surachet.pich@kmutt.ac.th>
2022-08-29 21:12:51 +0700

    Update : fillStartWord-V.1

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,12 @@
-const { template } = require('@babel/core')
+// const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
   //code here
+  return (word === startWord ? word : word == null || undefined ? 'undefined' :  startWord + word )
 }
-module.exports = fillStartWord
+// module.exports = fillStartWord
+console.log(fillStartWord("1-2565-","Hello World"))
+console.log(fillStartWord("JS", "beginner" ))
+console.log(fillStartWord("first", "firststep" ))
+console.log(fillStartWord("first", null ))
+console.log(fillStartWord("first", undefined ))
