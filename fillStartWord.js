3feccad1c14d44192b8e542a0df6c993cd8040f0
dodoza1415 <surachet.pich@kmutt.ac.th>
2022-08-29 21:18:02 +0700

    Update : fillStartWord_v.2

--- fillStartWord.js
+++ fillStartWord.js
@@ -5,8 +5,8 @@ function fillStartWord(startWord, word) {
   return (word === startWord ? word : word == null || undefined ? 'undefined' :  startWord + word )
 }
 // module.exports = fillStartWord
-console.log(fillStartWord("1-2565-","Hello World"))
-console.log(fillStartWord("JS", "beginner" ))
-console.log(fillStartWord("first", "firststep" ))
-console.log(fillStartWord("first", null ))
-console.log(fillStartWord("first", undefined ))
+//console.log(fillStartWord("1-2565-","Hello World"))
+//console.log(fillStartWord("JS", "beginner" ))
+//console.log(fillStartWord("first", "firststep" ))
+//console.log(fillStartWord("first", null ))
+//console.log(fillStartWord("first", undefined ))
