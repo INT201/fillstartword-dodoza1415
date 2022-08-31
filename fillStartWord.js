03b42c00ab3f299d9c391cde062d2b3060081cc8
dodoza1415 <surachet.pich@kmutt.ac.th>
2022-08-31 22:18:23 +0700

    Update : fillStartWord > localeCompare

--- fillStartWord.js
+++ fillStartWord.js
@@ -3,7 +3,7 @@
 function fillStartWord(startWord, word) {
   //code here
 
-  return (word == null || undefined ? 'undefined' : word == startWord ? word : startWord + word )
+  return (word == null || undefined ? 'undefined' : word.localeCompare(startWord) ? word : startWord + word )
 }
 // module.exports = fillStartWord
 console.log(fillStartWord("1-2565-","Hello World"))
